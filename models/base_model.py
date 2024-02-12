283b31ec6313290fc168c4563b4d8ad8bd63caf4
diasbouk <boukdsaid@gmail.com>
2024-02-12 01:40:14 +0100

    task 1

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,43 @@
 Base model for all classes
 """
 
+import uuid
+import datetime
+import json
+from datetime import datetime
+
 
 class BaseModel():
     """
     Base model
     """
-    def __init__():
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Class string presentation
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the time to last update
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Saves class info into a dictionary
+        """
+        class_dict = {}
+        for element in self.__dict__:
+            class_dict[element] = self.__dict__[element]
+        class_dict["__class__"] = self.__class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
+        return class_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+"""
+Main file for our project instances info
+"""
