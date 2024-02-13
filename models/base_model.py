c0f9202e211a7a2a5874957e9a5bcb2be63771d5
diasbouk <boukdsaid@gmail.com>
2024-02-13 12:56:54 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,22 @@ class BaseModel():
     Base model
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if (kwargs is not None):
+            for key in kwargs:
+                if key == "__class__":
+                    pass
+                if key == "id":
+                    self.id = kwargs[id]
+                if key == "created_at":
+                    self.created_at = datetime.striptime(kwargs[key], "\%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    self.updated_at = datetime.striptime(kwargs[key], "\%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
 import unittest
-from from models.base_model import BaseModel
+from AirBnB_clone.models import *
 
+
+def is_string(input):
+    if type(input) == str:
+        return True
+    else:
+        return False
+
+class testBaseModel(unittest.TestCase):
+    def test_created(self):
+        result = base_model.BasModel.__str__()
+        self.assertIsNotNone(result)
+        is_it = is_string(result)
+        self.assertTrue(is_it)
+
+if __name__ == "__main__":
+    unittest.main()
