665102fba6a0925ec5363e719860c91259caa609
diasbouk <boukdsaid@gmail.com>
2024-02-13 13:05:16 +0100

    task 4 fixed

--- models/base_model.py
+++ models/base_model.py
@@ -9,34 +9,56 @@ import json
 from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """
     Base model
     """
 
     def __init__(self, *args, **kwargs):
-        
+        """
+        Init methode of our object
+
+        Args:
+             *args: Additional positional arguments (if any).
+             **kwargs: Additional keyword arguments (if any).
+
+         Attributes:
+             id (str): The unique identifier of the object.
+             created_at (datetime): time when has been created
+             updated_at (datetime): time when has been updated
+        """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if (kwargs is not None):
+        if kwargs is not None:
             for key in kwargs:
                 if key == "__class__":
                     pass
-                if key == "id":
-                    self.id = kwargs[id]
-                if key == "created_at":
-                    self.created_at = datetime.striptime(kwargs[key], "\%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
-                    self.updated_at = datetime.striptime(kwargs[key], "\%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "id":
+                    self.id = kwargs[key]
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(
+                        kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
         Class string presentation
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
