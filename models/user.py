d2bbbdf2de1ee469138d1edf78dfebfeacc741ac
diasbouk <boukdsaid@gmail.com>
2024-05-19 10:42:29 +0100

    user class added ( Task 8 )

---
+++ models/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Docs 
+and imports
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User Class that inherits from
+    BaseModel
+
+     Attributes:
+     
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+
+    """
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
