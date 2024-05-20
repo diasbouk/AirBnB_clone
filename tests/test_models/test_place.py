af1c73700bbcea9f37a116e95c75f691f2e29396
diasbouk <boukdsaid@gmail.com>
2024-05-20 20:29:27 +0100

    place test

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Import modules and stuff here
+
+import unittest
+from models import place
+
+
+class TestPlace(unittest.TestCase):
+    # Class to test place
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
