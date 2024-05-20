81c1c12484b058ff28f79edbd7c8955606bc63fa
diasbouk <boukdsaid@gmail.com>
2024-05-20 20:32:36 +0100

    test place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,7 +7,9 @@ from models import place
 
 class TestPlace(unittest.TestCase):
     # Class to test place
-    pass
+    def test_random(self):
+        # test empty
+        self.assertEqual(1, 1)
 
 if __name__ == "__main__":
     unittest.main()
