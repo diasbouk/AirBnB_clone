e3f11361ff0b4d6a4602d1c12c05b4a1c3cd9535
diasbouk <boukdsaid@gmail.com>
2024-05-21 02:27:38 +0100

    amenity class present

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Import modules and stuff here
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    class Amenity
+    Public class attributes:
+
+        name: string - empty string
+    """
+
+    name = ""
