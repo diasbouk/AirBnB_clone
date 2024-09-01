a470ada958b7a4e64b282b63b7ab4a6b1bcaf570
diasbouk <boukdsaid@gmail.com>
2024-09-01 13:01:46 +0100

    Tests done

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,11 +5,14 @@ import unittest
 from models.base_model import BaseModel
 import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     # Class for Basemodel Tests
     def test_id(self):
         new_instance = BaseModel()
         self.assertEqual(type(new_instance.id), str)
+
     def test_times(self):
         new_instance = BaseModel()
-        self.assertEqual(type(new_instance.created_at), type(new_instance.updated_at))
+        self.assertEqual(type(new_instance.created_at),
+                         type(new_instance.updated_at))
