2310dee74541aeb771b7f1d934c0ff3308a65c52
diasbouk <boukdsaid@gmail.com>
2024-09-01 13:00:39 +0100

    style done

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     # BaseModel class for all other classes
     #     in tht project
@@ -9,13 +10,15 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
         # String representation of the instance
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}" 
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         # Saves the current changes
         self.updated_at = datetime.now()
+
     def to_dict(self):
         # Returns dict of the class
         return (self.__dict__)
-
