45143c295744b1d5554f350a7723d7921a0faddf
diasbouk <boukdsaid@gmail.com>
2024-09-03 20:19:17 +0100

    models/__init__.py for storage instance creation

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Creating New instance for the storage
+    class
+"""
 from models.engine import file_storage
+# init instance
 storage = file_storage.FileStorage()
+# Reload it
 storage.reload()
