a734498d26e9f8084331e7831e38d4af9411bcd0
diasbouk <boukdsaid@gmail.com>
2024-09-04 12:17:26 +0100

    Test for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,21 +5,11 @@
 """
 from unittest import TestCase
 from console import HBNBCommand
+import os
 
 
 class TestConsole(TestCase):
     """ Docs for Test """
 
     def test_help(self):
-        # To test help command
-        file = open('file', 'r')
-        out = open('out', 'a')
-        console = HBNBCommand(stdin=file, stdout=out)
-        console.use_rawinput = False
-        console.cmdloop()
-        out.close()
-        out = open('out', 'r')
-        print('------------------------------------------->')
-        print(out.read())
-        file.close()
-        out.close()
+        pass
