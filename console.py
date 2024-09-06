e56ad67559fa2a73ff07b801e7806ee14a8048e0
diasbouk <boukdsaid@gmail.com>
2024-09-06 13:11:41 +0100

    do_show ongoing

--- console.py
+++ console.py
@@ -7,62 +7,75 @@ import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.__init__ import storage
+import json
 
 
 class HBNBCommand(cmd.Cmd):
-    intro = ''
-    prompt = '(hbnb) '
+    intro = ""
+    prompt = "(hbnb) "
     file = None
-    models = ['BaseModel']
-    storage.reload()
-    reloaded_objs = storage.all()
-
-    def do_quit(self, args=''):
-        'Quit command to exit the program'
+    models = ["BaseModel"]
+    instances = []
+    def do_quit(self, *args):
+        "Quit command to exit the program"
         return True
 
-    def do_EOF(self, args=''):
-        'EOF command to exit the program'
+    def do_EOF(self, args):
+        "EOF command to exit the program"
         return True
 
     def emptyline(self):
-        'When an emptyline is entered, no action is taken'
-        return ''
+        "When an emptyline is entered, no action is taken"
+        return False
 
     def do_create(self, *args):
-        'Creates an new instance'
-        if args[0] == '':
-            print('** class name missing **')
-            return ''
+        "Creates an new instance"
+        if args[0] == "":
+            print("** class name missing **")
+            return ""
         if args is not None:
             if args[0] not in self.models:
-                print('** class doesn\'t exist **')
-                return ''
+                print("** class doesn't exist **")
+                return ""
             try:
-                bm = eval(f'{args[0]}()')
+                bm = eval(f"{args[0]}()")
                 print(bm.id)
                 bm.save()
             except Exception as Err:
                 print(Err)
 
-    def do_show(self, arg=''):
+    def do_show(self, args):
+        "Shows info of an instance using its id"
 
-        print(el for el in self.reloaded_objs)
-        print(self.reloaded_objs)
+        list = args.split()
+        if len(list) == 0:
+            print('** class name missing **')
+            return ''
+        if list[0] != 'BaseModel':
+            print('** class doesn\'t exist **')
+            return ''
+        if len(list) < 2:
+            print('** instance id missing **')
+            return ''
+        try:
+            with open ('file.json', 'r') as file:
+                objs = json.load(file)
+                i = 0
+                for key,val in objs.items():
+                    self.instances.append(BaseModel(**val))
+        except Exception as Err:
+            print(Err)
+        ids = [el.id for el in self.instances]
+        if list[1] not in ids:
+            print('** no instance found **')
+            return ''
+        for ins in self.instances:
+            if ins.id == list[1]:
+                print(ins.__str__)
 
-        # ids = [obj.get('id') for obj in self.reloaded_objs]
-        # args = arg.split(' ')
-        # if args[0] == '':
-        #     print('** class name missing **')
-        #     return ''
-        # if args[0] not in self.models:
-        #     print('** class doesn\'t exist **')
-        # if args[1] is None:
-        #     print('** instance id missing **')
-        #     return ''
-        # if args[1] not in ids:
-        #     print('** no instance found **')
-        # print(self.reloaded_objs)
+    def do_sow(self, args=''):
+        'tests for show'
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
