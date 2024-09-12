0db06e905c6f782940fad4dd759cc77c5019110d
diasbouk <boukdsaid@gmail.com>
2024-09-12 02:08:10 +0100

    Changed time handling and formats in the basemodel, using strftime and strptime , utcnow, instead of isoformat, fromisoformat, now()

--- models/base_model.py
+++ models/base_model.py
@@ -28,14 +28,14 @@ class BaseModel:
                 if key == "__class__":
                     continue
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
-                    setattr(self, key, datetime.fromisoformat(value))
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         if kwargs == {}:
             models.storage.new(self)
 
@@ -50,7 +50,7 @@ class BaseModel:
         save: The save method
         saves the current instance
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
@@ -63,6 +63,6 @@ class BaseModel:
         for key in old_dict.keys():
             new_dict[key] = old_dict[key]
         new_dict["__class__"] = str(self.__class__.__name__)
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["created_at"] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        new_dict["updated_at"] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         return new_dict
